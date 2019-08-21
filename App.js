4f803823c292cd955fd55594d299c227a6650e90
王晶晶 <857714819@qq.com>
2019-08-21 14:26:53 +0800

    test android

--- App.js
+++ App.js
@@ -60,9 +60,19 @@ class App extends Component {
                 // console.log(result.page);
                 console.log(Platform.OS)
                 if (Platform.OS === 'android') {
-                    alert(result.name + result.man + result.age + result.money + result.height);
+                    alert(result.name + result.man + result.age + result.money + result.height + result.city);
+                    console.log(typeof(result.type))
+                    console.log(typeof(result.man))
+                    console.log(typeof(result.age))
+                    console.log(typeof(result.money))
+                    console.log(typeof(result.height))
+                    console.log(typeof(result.city))
                 }else{
                     alert(result.type + result.sessionId + result.money + result.page);
+                    console.log(typeof(result.type))
+                    console.log(typeof(result.sessionId))
+                    console.log(typeof(result.money))
+                    console.log(typeof(result.page))
                 }
             })
         // },10000);
