617f0c3bac37eec89ec7c478dfb7983e6614d258
tangbinbin <tangbinbin@btte.net>
2019-08-26 15:46:27 +0800

    新增页面，2级导航路由设置

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import React, {Fragment,Component} from 'react';
+import React, {Component} from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from "./HomeScreen";
+import DetailPage from "./DetailPage";
 
 const AppNavigator = createStackNavigator({
-  HomeScreen: HomeScreen
-});
+      Home: HomeScreen,
+      Details: DetailPage
+    },
+    {
+      initialRouteName: "Home"
+    }
+)
 const AppContainer = createAppContainer(AppNavigator);
 export default class App extends Component {
   render() {
